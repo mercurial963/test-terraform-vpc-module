d243f76e88f3f9b7ea4e5aaeaf7945d5c17ac43b
pamnattharat lersiripong <pamnattharatl@arv.co.th>
2021-12-14 01:01:18 +0700

    first commit

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 provider "aws" {
   region     = var.aws_account.region
-  profile    = var.aws_account.profile
   access_key = var.aws_account.access_key
   secret_key = var.aws_account.secret_key
-  default_tags {
-    tags = var.project_info
-  }
 }
