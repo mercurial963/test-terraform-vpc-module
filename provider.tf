0d3fbdb35415c50ee7c0c813a498c5af98246a74
pamnattharat lersiripong <pamnattharatl@arv.co.th>
2021-12-14 11:59:20 +0700

    first commit

--- provider.tf
+++ provider.tf
@@ -6,3 +6,27 @@ provider "aws" {
 terraform {
   experiments = [module_variable_optional_attrs]
 }
+
+// terraform {
+//   backend "remote" {
+//     hostname = "app.terraform.io"
+//     organization = "mercurial963"
+
+//     workspaces {
+//       name = "test-terraform-vpc-module"
+//     }
+//   }
+// }
+
+terraform {
+  backend "s3" {
+    bucket         = "test-bucket-for-tf-state"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+    // dynamodb_table = "test-dynamodb-table-for-tf-state"
+    access_key = "AKIAUC7QGW377WKZMN5E"
+    secret_key = "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"
+  }
+}
+  
